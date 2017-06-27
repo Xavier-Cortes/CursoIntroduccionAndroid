1b82215b42e655583ac937a8da506314cfb374e7
Your Name <informatica@mobba.es>
2017-06-27 15:47:46 +0200

    Initial commit

---
+++ app/src/main/java/com/xavicortes/myaplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.xavicortes.myaplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int numero;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.v(TAG,"OnCreate");
+        setContentView(R.layout.activity_main);
+        numero = 0;
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "OnStart");
+        numero = 1;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "OnStop");
+        numero = 2;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "OnDestroy");
+        numero = 3;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "OnPause");
+        numero = 4;
+    }
+}
